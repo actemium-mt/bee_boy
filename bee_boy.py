4328fa7baf30cf9c414d12f5f0d130a796f86c2b
ikallelactemium <ismail.kallel@actemium.com>
2023-07-28 15:19:29 +0200

    ajout du mot de passe et de la clé api

--- bee_boy.py
+++ bee_boy.py
@@ -3,7 +3,6 @@ from fuzzywuzzy import fuzz, process
 import openai
 import streamlit as st
 import streamlit_chat
-#test push
 
 def get_completion(prompt):
     response = openai.Completion.create(
@@ -28,42 +27,49 @@ def find_similar_items_smart(input,data_base):
     return liste_elements
 
 def main():
-    df_historique_panne = pd.read_excel('Nouveau document texte.xlsx') 
-    liste_commentaire  = df_historique_panne["COMMENTAIRE "].tolist()
-    liste_commentaire_non_null = []
-    for i in range(len(liste_commentaire)):
-        if str(liste_commentaire[i]) != 'nan':
-            liste_commentaire_non_null.append(liste_commentaire[i])
-    resultats = process.extract("probleme", liste_commentaire_non_null, limit=4000)
-    liste_commentaire_final = []
-    for resultat in resultats:
-        liste_commentaire_final.append(resultat[0])
-    api_key ="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"
-    openai.api_key = api_key
-    st.title("Bee Boy actemium assistant")
-    if "messages" not in st.session_state:
-        first_txt = """Salut ! 👋 Je suis là pour transformer le dépannage en une tâche facile pour vous. Parlons de votre problème et découvrons les meilleures solutions ensemble."""
-        st.session_state.messages = [{"role": "assistant", "message": first_txt,"content" :""}]
-        
+    correct_password = "lalala"
+    password = st.text_input("Entrer le mot de passe :", type="password")
+    
+    api_key = st.text_input("Entrer la clé d'API :", type="password")
+    if password == correct_password and len(api_key)>50:
+        df_historique_panne = pd.read_excel('Nouveau document texte.xlsx') 
+        liste_commentaire  = df_historique_panne["commentaires"].tolist()
+        liste_designation = df_historique_panne["designation"].tolist()
+        liste_description = df_historique_panne["descriptions"].tolist()
+        liste_organe_machine = df_historique_panne["organe_machine"].tolist()
+        liste_commentaire_final = []
+        for i in range(len(liste_commentaire)):
+            liste_commentaire_final.append("organe machine : "+str(liste_organe_machine[i])+"designation de la panne : "+str(liste_designation[i])+" descriptions de la panne : "+str(liste_description[i])+" commentaire sur la panne : "+str(liste_commentaire[i]))
+        #api_key ="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"
+        openai.api_key = api_key
+        st.title("Bee Boy actemium assistant")
+        if "messages" not in st.session_state:
+            first_txt = """Salut ! 👋 Je suis là pour transformer le dépannage en une tâche facile pour vous. Parlons de votre problème et découvrons les meilleures solutions ensemble."""
+            st.session_state.messages = [{"role": "assistant", "message": first_txt,"content" :""}]
+            
 
-    user_input = st.chat_input()
-    if user_input:
-        st.session_state.messages.append({"role": "user", "message": user_input,"content":""})
-        response = ""
-        prompt = "est ce que ceette phrase : " +user_input+" contient des  mots techniques en relation avec la maintenace industrielle? repond seulement par oui ou non"
-        response1 = get_completion(prompt)
-        if "oui" in response1.lower():
-            similar_data  = find_similar_items_smart(user_input,liste_commentaire_final)
-            prompt = "en se basant seulement sur ça " + str(similar_data) + " tire tout les actions les plus pertinentes en relation avec"+user_input+"et presente les comme des listes d'actions qu'on peut faire en reformulant une reponse technique et developpent bien chaque actions"
-            response = get_completion(prompt)
-        else:
-            response = get_completion(user_input)
-        st.session_state.messages.append({"role": "assistant", "message":response,"content":""})
-    if "messages" in st.session_state:
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.write(message["message"])
-
+        user_input = st.chat_input()
+        if user_input:
+            st.session_state.messages.append({"role": "user", "message": user_input,"content":""})
+            response = ""
+            prompt = "est ce que ceette phrase : " +user_input+" contient des  mots techniques en relation avec la maintenace industrielle? repond seulement par oui ou non"
+            response1 = get_completion(prompt)
+            if "oui" in response1.lower():
+                
+                key_words = get_completion("detecte les mots clés de cette phrase : " +user_input+" .retourne seulement les mots clés")
+                print(key_words)
+                similar_data  = find_similar_items_smart(key_words,liste_commentaire_final)
+                prompt = "en se basant seulement sur ça " + str(similar_data) + " c'est quoi les actions les plus pertinentes à realise en relation avec"+user_input+"et presente les comme des listes d'actions qu'on peut faire en reformulant une reponse technique et developpent bien chaque actions"
+                response = get_completion(prompt)
+            else:
+                response = get_completion(user_input)
+            st.session_state.messages.append({"role": "assistant", "message":response,"content":""})
+        if "messages" in st.session_state:
+            for message in st.session_state.messages:
+                with st.chat_message(message["role"]):
+                    st.write(message["message"])
+    else:
+        st.error("Mot de passe incorrecte ou clé invalide.")
 main()
 
 
