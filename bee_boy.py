80a350b90e02261c66b17b0ab72dba1b1d6b6bb9
ikallelactemium <ismail.kallel@actemium.com>
2023-08-16 08:15:19 +0200

    ajout de la section avis et commentaire

--- bee_boy.py
+++ bee_boy.py
@@ -3,6 +3,8 @@ from fuzzywuzzy import fuzz, process
 import openai
 import streamlit as st
 import streamlit_chat
+from streamlit_star_rating import st_star_rating
+from PIL import Image
 #testmerge
 
 def get_completion(prompt):
@@ -51,11 +53,18 @@ def find_similar_items_smart(input,data_base,liste_commentaire,liste_description
     return liste_elements
 
 def main():
-
+    image = Image.open('Boy-removebg-preview.png')
     correct_password = "lalala"
     password = st.text_input("Entrer le mot de passe :", type="password")
-    
     api_key = st.text_input("Entrer la clé d'API :", type="password")
+    first_txt = """Salut ! 👋 Je suis là pour transformer le dépannage en une tâche facile pour vous. Parlons de votre problème et découvrons les meilleures solutions ensemble."""
+    sorry_text = """Je suis votre assistant de maintenance, et je peux répondre uniquement à des questions techniques liées à la maintenance industrielle."""
+    df_commentaire = pd.read_excel("commentaire.xlsx")
+    commentaire_liste = df_commentaire["Commentaire"].tolist()
+    avis_liste = df_commentaire["Avis"].tolist()
+    question_liste = df_commentaire["Question"].tolist()
+    reponse_liste = df_commentaire["Réponse"].tolist()
+    
     if password == correct_password and len(api_key)>50:
         df_historique_panne = pd.read_excel('Nouveau document texte.xlsx') 
         liste_commentaire  = df_historique_panne["commentaires"].tolist()
@@ -68,18 +77,19 @@ def main():
         
         openai.api_key = api_key
         st.title("Bee Boy actemium assistant")
+        st.image(image)
         st.title("version 1.1")
         if "messages" not in st.session_state:
-            first_txt = """Salut ! 👋 Je suis là pour transformer le dépannage en une tâche facile pour vous. Parlons de votre problème et découvrons les meilleures solutions ensemble."""
+            
             st.session_state.messages = [{"role": "assistant", "message": first_txt,"content" :""}]
             
 
-
+        response = ""
         user_input = st.chat_input()
         if user_input:
             st.toast('Vérification en cours...')
             st.session_state.messages.append({"role": "user", "message": user_input,"content":""})
-            response = ""
+            
             prompt = "est ce que cette phrase : " +user_input+" contient des  mots techniques en relation avec la maintenace industrielle? repond seulement par oui ou non"
             response1 = get_completion(prompt)
             if "oui" in response1.lower():
@@ -88,20 +98,49 @@ def main():
                 print(key_words)
                 similar_data  = find_similar_items_smart(key_words,liste_commentaire_final,liste_commentaire,liste_description)
                 
-                prompt = "en se basant seulement sur cet historique de panne et de actions faites : " + str(similar_data) + " tire toutes les actions réalisées en relation avec  le probleme : "+user_input+". je veux la liste  avec les actions realise reformulés pour le probleme."
+                prompt = "en se basant seulement sur cet historique de panne et de actions faites : " + str(similar_data) + " tire toutes les actions réalisées en relation avec  le probleme : "+user_input+". je veux la liste  avec les actions realise reformulés pour le probleme.LISTE EN BULLET POINT AVEC DES SAUTS DE LIGNES"
                 response = get_completion(prompt)
                 print(response)
                 
             else:
                 st.toast("c'est pas une question technique liée à la maintenance industrielle",icon = "😵")
-                response = "Je suis votre assistant de maintenance, et je peux répondre uniquement à des questions techniques liées à la maintenance industrielle."
+                response = sorry_text
             st.session_state.messages.append({"role": "assistant", "message":response,"content":""})
+
         if "messages" in st.session_state:
-            for message in st.session_state.messages:
+            i = 0
+            j = 0
+            
+            for message in st.session_state.messages: 
                 with st.chat_message(message["role"]):
                     st.write(message["message"])
+                    if message["message"] != first_txt and message["role"] == "assistant" and message["message"]!=sorry_text:
+                        form_key = "rating_form" + str(j+1000)
+                        with st.form(key = form_key):
+                            col1,col2 = st.columns(2)
+                            with col1:
+                                st.markdown("<h3>Commentaire</h3>", unsafe_allow_html=True)
+                                commentaire = st.text_area("",key = i,placeholder = "Que pensez-vous de la réponse de Bee Boy",label_visibility="collapsed")
+                            with col2:
+                                stars = st_star_rating("Avis", maxValue=5, defaultValue=1, key=i+1,emoticons = True)
+                            submitted = st.form_submit_button("Envoyer")
+                            if submitted:
+                                commentaire_liste.append(commentaire)
+                                avis_liste.append(stars)
+                                question_liste.append(st.session_state.messages[j-1]["message"])
+                                reponse_liste.append(message["message"])
+                                new_df_commentaire = pd.DataFrame({'Question':question_liste,'Réponse':reponse_liste,'Commentaire':commentaire_liste,'Avis':avis_liste})
+                                new_df_commentaire.to_excel("commentaire.xlsx")
+                                st.success("Merci pour votre retour, c'est très important pour l'amélioration de votre assistant préféré Bee Boy! 😀")
+
+                i=i+2
+                j=j+1
+            
+
     else:
         st.error("Mot de passe incorrecte ou clé invalide.")
+    
+        
 main()
 
 
