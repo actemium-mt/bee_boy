d7a031bdacdf1afab7b80ea5dacc9d4fdb3a84a2
ikallelactemium <119849253+ikallelactemium@users.noreply.github.com>
2024-01-12 10:04:39 +0100

    Update bee_boy.py (with the new da vinci model)

--- bee_boy.py
+++ bee_boy.py
@@ -14,7 +14,7 @@ import plotly.graph_objects as go
 
 def get_completion(prompt):
     response = openai.Completion.create(
-        model="text-davinci-003",
+        model="gpt-3.5-turbo-instruct",
         prompt=prompt,
         temperature=0.9,
         max_tokens=700,
